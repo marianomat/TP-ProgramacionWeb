0cca654ed0b69a4dc0a6aff39ef0524d37cf5e75
Mariano <marianopereyra95@gmail.com>
2021-11-07 12:36:09 -0300

    Faltaron commitear estos archivos del back

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,12 +11,13 @@ class Turno(models.Model):
     # Falta poner las condiciones dentro de los parentesis como los max_length, etc. Cuando modifiquemos esto va a
     # aparecer el error del video https://www.youtube.com/watch?v=SSbmewgBZQc&list=PLquoPC3ciXW_Yih1lwIrnDxyE0ZdsU4lr
     # &index=27
-    hour = models.DateTimeField()
-    description = models.TextField()
-    is_payed = models.BooleanField()
-    patient_name = models.TextField()
-    patient_lastName = models.TextField()
-    patient_phone = models.IntegerField()
-    patient_email = models.TextField()
+    hour = models.DateTimeField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    is_payed = models.BooleanField(blank=True, null=True)
+    doctor = models.TextField(blank=True, null=True)
+    patient_name = models.TextField(blank=True, null=True)
+    patient_lastName = models.TextField(blank=True, null=True)
+    patient_phone = models.IntegerField(blank=True, null=True)
+    patient_email = models.TextField(blank=True, null=True)
 
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'drf_yasg',
     'rest_framework_simplejwt',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,3 +140,5 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
