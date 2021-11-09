b90befa16cf527f461f811813010aa58a8747c22
marianomat <marianopereyra95@gmail.com>
2021-11-09 16:43:57 -0300

    Arreglo bug registropacientes.js

--- turnos-react/src/RegistroPacientes/RegistroPacientes.js
+++ turnos-react/src/RegistroPacientes/RegistroPacientes.js
@@ -1,42 +1,40 @@
-import './App.css';
 import React from "react";
 import './RegistroPacientes.css'
 const RegistroPacientes = () => {
     return (
-                <React.Fragment>
-                <title>Página Turnos</title>
-                <div>
-                  <h1 className="custom_title center">
-                    REGISTRO DE TURNOS
-                  </h1>
+        <div className="registropaciente-contenedor">
+            <title>Página Turnos</title>
+            <div>
+              <h1 className="registropaciente-custom_title registropaciente-center">
+                REGISTRO DE TURNOS
+              </h1>
+            </div>
+            <div>
+              <h2  class="registropaciente-tit">
+                Complete el siguiente formulario
+              </h2>
+              <form class="registropaciente-tit">
+                <div class="registropaciente-input-grupo">
+                  <label for="name">Nombre:</label>
+                  <input type="text" id="name" name="user_name"/>
                 </div>
-                <div>
-                  <h2  class="tit">
-                    Complete el siguiente formulario
-                  </h2>
-                  <form class="tit">
-                    <div class="input-grupo">
-                      <label for="name">Nombre:</label> 
-                      <input type="text" id="name" name="user_name"/>
-                    </div>
-                    <div class="input-grupo">   
-                      <label for="lastname">Apellido:</label>
-                      <input type="text" id="lastname" lastname="user_lastname"/>
-                    </div>
-                    <div class="input-grupo">  
-                      <label for="number">Número celular:</label>
-                      <input type="text" id="number" number="user_number"/>
-                    </div>
-                    <div class="input-grupo">  
-                      <label for="mail">Correo electrónico:</label>
-                      <input type="email" id="mail" name="user_mail"/>
-                    </div>
-                    <button type="submit"> Enviar formulario </button>
-                  </form>
+                <div class="registropaciente-input-grupo">
+                  <label for="lastname">Apellido:</label>
+                  <input type="text" id="lastname" lastname="user_lastname"/>
                 </div>
-                </React.Fragment>
-            
-                );
+                <div class="registropaciente-input-grupo">
+                  <label for="number">Número celular:</label>
+                  <input type="text" id="number" number="user_number"/>
+                </div>
+                <div class="registropaciente-input-grupo">
+                  <label for="mail">Correo electrónico:</label>
+                  <input type="email" id="mail" name="user_mail"/>
+                </div>
+                <button type="submit"> Enviar formulario </button>
+              </form>
+            </div>
+        </div>
+    );
 }
 
 export default RegistroPacientes;
