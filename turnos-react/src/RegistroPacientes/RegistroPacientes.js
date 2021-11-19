688b61dce5d2e02bfe6f748b882c7ab49683dca9
LucasFasolato <ldfasolato@mail.austral.edu.ar>
2021-11-18 21:21:26 -0300

    Commit RegistroPA

--- turnos-react/src/RegistroPacientes/RegistroPacientes.js
+++ turnos-react/src/RegistroPacientes/RegistroPacientes.js
@@ -7,6 +7,7 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getToday} from "../utils/helpers";
 import {Link} from "react-router-dom";
+import Turnos from "../perfil/componentes-perfil/Turnos/Turnos";
 
 function RegistroPacientes(props) {
   const alert = useAlert()
@@ -15,8 +16,26 @@ function RegistroPacientes(props) {
   const [pago, setPago] = useState({})
   const [cantidadTurnos, setCantidadTurnos] = useState(0)
   const [id, setid] = useState()
- /*  const [turnoseleccionado, setTurnoSeleccionado] = useState ({}) */
-function optionpago() {
+  const [turnoseleccionado, setTurnoSeleccionado] = useState ({}) 
+
+function activaropciones()
+{
+  if (document.formu.Pago.value == true) {
+    document.formu.pagos.disabled = false;
+     }else{
+    if (document.formu.Pago.value == false) {
+             document.formu.pagos.disabled = true;
+    }
+    }
+} 
+
+/* function desactivaropciones()
+{
+  return(
+  document.getElementById("pagos").disabled="true"
+  )} */
+
+/* function optionpago() {
   let turnoselec = ""
     for (let i in turno){
       if (i.id == id){
@@ -37,11 +56,10 @@ function optionpago() {
           <input id="codigopago" value={pago.payment_code ? pago.payment_code: ""} placeholder="- - - -"
                   onChange={(e) => setPago({...pago, payment_code:e.target.value})}/>
           </div>
+          {pago.turno = id}
       </div>
-    )}
-
-  
-}
+    )} 
+} */
   const fetchTurnos = () => {
     ///No olvidar la barra al final de turnos
     httpGet("api/turnos/")
@@ -70,7 +88,15 @@ function optionpago() {
     }).catch((err) => alert.show("Error",{
         type: "error"
     }))
-    httpPost("api/pagos/").then((res) => {
+    let pagopost = pago
+    pagopost.turno = id
+    console.log(pagopost)
+    let pagotest = {
+      monto: 500, 
+      payment_code: 200,
+      turno: 4
+    }
+    httpPost("api/pagos/", pagotest).then((res) => {
       alert.show('Pago modificado correctamente',{
           type: "success"
       })
@@ -101,7 +127,7 @@ function optionpago() {
               <h2  className="registropaciente-tit">
                 Complete el siguiente formulario
               </h2>
-              <form className="registropaciente-tit" onSubmit ={(e) => editTurno(e)} >
+              <form className="registropaciente-tit" onSubmit ={(e) => editTurno(e)} name="formu">
                 <div className="registropaciente-input-grupo">
                   <label for="nombre">Nombre:</label>
                   <input id="nombre" value={turno.patient_name ? turno.patient_name : ""} placeholder="Nombre"
@@ -124,28 +150,44 @@ function optionpago() {
                 </div>
 
                 <div className="registropaciente-input-grupo">
-                  <label for="pagoturno">Pago de turno</label>
+                  <label for="pagoturno">¿Pagar ahora?</label>
                     <select name="Pago" id='pagoturno' className="botons" value = {turno.is_payed}
                       onChange={(e) => setTurno({...turno, is_payed:e.target.value})}> 
                       {/* /* onChange={optionpago}> */}
-                        <option value= {true}>Pagado</option>
-                        <option value= {false}>No pagado</option>
+                        <option value= {true} onClick={activaropciones}>SI</option>
+                        <option value= {false} onClick={activaropciones}>NO</option>
                     </select>    
-                    {
-                      optionpago ()
-                    } 
                </div>
+
+               
+
+               <div>
+                <div className="registropaciente-input-grupo">
+                  <label for="pagos">Monto:</label>
+                  <input id="pagos" value={pago.monto ? pago.monto : ""} placeholder="$" disabled = {false} /* {document.getElementById("pagoturno").value} */
+                    onChange={(e) => setPago({...pago, monto:e.target.value})}/>
+                </div>
+                  
+                <div className="registropaciente-input-grupo">
+                  <label for="pagos">Codigo Pago</label>
+                  <input name= "pagos" id="pagos" value={pago.payment_code ? pago.payment_code: ""} placeholder="- - - -" disabled = {false} /* {document.getElementById("pagoturno").value} */
+                    onChange={(e) => setPago({...pago, payment_code:e.target.value})}/>
+                </div>
+                {pago.turno = id}
+              </div>
                
                <div className="registropaciente-input-grupo">
                   <label for="fechaturno">Fecha de turnos disponibles</label>
                   <select name="Fecha" id='fechaturno' className="botons" value = {turno.hour}
                     onChange={(e) => setid(e.target.value)}>
                       {registropacientes.map(turno => {
+                        if (turno.is_taken == false){
                         return(
                           <option value= {turno.id}> {turno.horario} </option>
-                        )   
+                        )} 
                     })}
                     </select>
+                {turno.is_taken = true}
                </div>
                 <button type="submit"> Enviar formulario </button>
               </form>
