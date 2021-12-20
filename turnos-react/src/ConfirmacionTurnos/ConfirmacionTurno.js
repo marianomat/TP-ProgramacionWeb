3315ce3271b0158eaea871735ee0205cb6d3e068
n4ikon <napinuelo@gmail.com>
2021-12-20 16:24:55 -0300

    estilo confirmar turnos

--- turnos-react/src/ConfirmacionTurnos/ConfirmacionTurno.js
+++ turnos-react/src/ConfirmacionTurnos/ConfirmacionTurno.js
@@ -49,7 +49,7 @@ function ConfirmacionPago() {
                 console.log(data)
                 setTurno(data);
             }).catch((err) => {
-                alert.show('Error obtenido los turnos, intente mas tarde!', {
+                alert.show('Error obteniendo los turnos, intente mas tarde!', {
                     type: "error"
                 })
             }
@@ -85,19 +85,21 @@ function ConfirmacionPago() {
     }, [preferenceId]);
 
     return (
-        <div>
+        <div className="confpagos-contenedor">
+            <div className= "confpagos-tit confpagos-center">
             <h1>Turno confirmado correctamente</h1>
             <h2>Datos del turno:</h2>
-            <ul>
-                <li>Doctor: {turno?turno.doctor.last_name + " " + turno.doctor.first_name :"Cargando"}</li>
-                <li>Horario: {turno?turno.hour:"Cargando"}</li>
-                <li>Nombre paciente: {turno?turno.patient_name:"Cargando"}</li>
-                <li>Apellido paciente: {turno?turno.patientlastName:"Cargando"}</li>
-                <li>Telefono paciente: {turno?turno.patient_phone:"Cargando"}</li>
+            <ul >
+                <li className= "confpagos-li">Doctor: {turno?turno.doctor.last_name + " " + turno.doctor.first_name :"Cargando"}</li>
+                <li className= "confpagos-li">Horario: {turno?turno.hour:"Cargando"}</li>
+                <li className= "confpagos-li" >Nombre paciente: {turno?turno.patient_name:"Cargando"}</li>
+                <li className= "confpagos-li" >Apellido paciente: {turno?turno.patientlastName:"Cargando"}</li>
+                <li className= "confpagos-li">Telefono paciente: {turno?turno.patient_phone:"Cargando"}</li>
             </ul>
             <h2>Puede abonar el turno haciendo click en pagar!</h2>
                 <h4>Costo: {turno?turno.price:"Cargando"}</h4>
                 <form onClick={pagarTurno} className="botonpago" method="GET"></form>
+                </div>
         </div>
     )
 }
